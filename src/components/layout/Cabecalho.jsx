fb0ab0b515e1d232a6ac60555004c7738551da03
Ignacio Neto <96788434+IgnacioNeto@users.noreply.github.com>
2022-10-10 12:00:39 -0300

    CSS externo
    CSS externo

--- src/components/layout/Cabecalho.jsx
+++ src/components/layout/Cabecalho.jsx
@@ -1,10 +1,16 @@
 import Menu from "../Menu";
 
+const estilos = {
+  color: "white",
+  backgroundColor: "black",
+};
+
 const Cabecalho = () => {
   return (
     /* CSS in JS */
+    /* CSS inline (usa propriedade style e sintaxe de objeto) */
     <header style={{ backgroundColor: "lightblue", textAlign: "center" }}>
-      <h1>Interface React</h1>
+      <h1 style={estilos}>Interface React</h1>
       <Menu />
     </header>
   );
