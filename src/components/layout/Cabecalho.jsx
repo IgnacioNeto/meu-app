4dff6d91a9120fccb888efd1d2cb00161148e73d
Ignacio Neto <96788434+IgnacioNeto@users.noreply.github.com>
2022-10-10 11:45:33 -0300

    CSS inline
    CSS inline

--- src/components/layout/Cabecalho.jsx
+++ src/components/layout/Cabecalho.jsx
@@ -2,7 +2,8 @@ import Menu from "../Menu";
 
 const Cabecalho = () => {
   return (
-    <header>
+    /* CSS in JS */
+    <header style={{ backgroundColor: "lightblue", textAlign: "center" }}>
       <h1>Interface React</h1>
       <Menu />
     </header>
