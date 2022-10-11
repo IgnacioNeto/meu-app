f9941cde86aa73b1d6a14d031f754663ce9e17bf
Ignacio Neto <96788434+IgnacioNeto@users.noreply.github.com>
2022-10-11 12:00:08 -0300

    Teste do bootstrap

--- src/components/layout/Conteudo.jsx
+++ src/components/layout/Conteudo.jsx
@@ -1,23 +1,22 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
 import Artigo from "../Artigo";
 
-const Conteudo = () => {
+function Conteudo() {
   return (
-    <main>
-      <section>
-        <h2>Conteúdo do site...</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
-          laboriosam qui quisquam nesciunt, incidunt quia repellendus dicta
-          omnis veniam expedita labore cum necessitatibus maxime? Quam a
-          dignissimos itaque illum et!
-        </p>
-
-        <Artigo />
-        <Artigo />
-        <Artigo />
-      </section>
-    </main>
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src="destaque.jpg" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
   );
-};
+}
 
 export default Conteudo;
